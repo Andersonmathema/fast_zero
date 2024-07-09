52edb01bc7f453d68009802fc369a4e796dfe4d5
Andersonmathema <ander.math89@gmail.com>
2024-07-09 20:18:00 -0300

    Adicionando rotas do CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,71 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (organização do teste)
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
+    # client = TestClient(app)  # Arrange (organização do teste)
     response = client.get('/')  # Act (ação)
     assert response.status_code == HTTPStatus.OK  # Assert (tudo ok)
     assert response.json() == {'message': 'Olá mundo'}  # Assert
 
 
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'email': 'test@test.com',
+            'password': 'password',
+        },
+    )
+    # Voltou o status correto?
+    assert response.status_code == HTTPStatus.CREATED
+    # Validar o UserPublic
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'testusername',
+                'email': 'test@test.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': '123',
+            'username': 'testusername2',
+            'email': 'test@test.com',
+            'id': 1,
+        },
+    )
+    assert response.json() == {
+        'username': 'testusername2',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
+
+
 # Exercicio Aula 02
-def test_read_ola_deve_retornar_ola_mundo():
-    client = TestClient(app)
+def test_read_ola_deve_retornar_ola_mundo(client):
     response = client.get('/ola')
     assert response.status_code == HTTPStatus.OK
     texto = """
